b9acf260999415da77d5031af1f48a667746eedf
Dmitry <AntiduPb@yandex.ru>
2017-07-14 21:30:55 +0300

    pass

--- server/main.c
+++ server/main.c
@@ -51,7 +51,7 @@ void* chatter(void* parms) {
     
     // Постараемся пока не пользовать мьютексы.
     struct chatter_parms *cast_parms = parms;
-    struct sock_list *sl = cast_parms->s32_list;
+    struct sock_list thr_sl = {}, *sl = cast_parms->s32_list;
 
     struct epoll_event ev, *events = malloc(sizeof (struct epoll_event) * MAX_CLIENTS_S);
     memset(events, 0, sizeof(struct epoll_event)*MAX_CLIENTS_S);
@@ -69,13 +69,16 @@ void* chatter(void* parms) {
     ev.data.fd = cast_parms->signal_sock;
     epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cast_parms->signal_sock, &ev);
     
+    // Один клиент уже есть - это основной поток с сигнальным сокетом.
+    // thr_sl.count++;
+    
     while (true) {
         
         number_fds = epoll_wait(epoll_fd, events, MAX_CLIENTS_S, -1);
         
         if (sl->count > 0) {
-            for (int i = 0; i < sl->count; i++) {
-                
+            for (int i = 0; i < sl->count && thr_sl.count < MAX_CLIENTS_S; i++) {
+                thr_sl.s32clients_fd[thr_sl.count + i] = sl->s32clients_fd[i];
             }
         }
     }
