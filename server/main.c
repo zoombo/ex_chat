77f792ef17ccf4b8e113ec4340f6a196f5a0e062
Dmitry <AntiduPb@yandex.ru>
2017-08-01 01:57:53 +0300

    some fix in comments ;)

--- server/main.c
+++ server/main.c
@@ -32,6 +32,7 @@
  */
 
 // Функция заглушка для неиспользуемых переменных.
+
 void pass(void *v) {
     return;
 }
@@ -42,48 +43,7 @@ void pass(void *v) {
  * 
  */
 
-void* chatter(void* parms) {
 
-    // TODO: Постараемся пока не пользовать мьютексы.
-
-    // Приводим принятую структуру к нужной нам.
-    struct chatter_parms *cast_parms = parms;
-    // thr_sl - структура которая будет хранить список сокетов
-    // с которыми работаем в данный момент.
-    struct sock_list thr_sl = {}, *sl = cast_parms->s32_list;
-
-    struct epoll_event ev = {}, *events = malloc(sizeof (struct epoll_event) * MAX_CLIENTS_S);
-    memset(events, 0, sizeof (struct epoll_event)*MAX_CLIENTS_S);
-
-    int epoll_fd, number_fds;
-    pass((void*)&number_fds);
-    
-    epoll_fd = epoll_create1(0);
-
-    if (epoll_fd == -1) {
-        printf("epoll_create1 error.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    ev.events = EPOLLIN;
-    ev.data.fd = cast_parms->signal_sock;
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cast_parms->signal_sock, &ev);
-
-    // Один клиент уже есть - это основной поток с сигнальным сокетом.
-    // thr_sl.count++;
-
-    while (true) {
-
-        number_fds = epoll_wait(epoll_fd, events, MAX_CLIENTS_S, -1);
-
-        if (sl->count > 0) {
-            for (int i = 0; i < sl->count && thr_sl.count < MAX_CLIENTS_S; i++) {
-                thr_sl.s32clients_fd[thr_sl.count + i] = sl->s32clients_fd[i];
-            }
-        }
-    }
-
-}
 
 int add_sock(struct sock_list *sl, int s32sock_fd) {
     // TODO: эта функция добавляет сокет подсоединившегося клиента к списку
@@ -133,7 +93,14 @@ int main(int argc, char** argv) {
     struct queue * clients[MAX_CLIENTS_S] = {};
     int clients_count = 0;
 
+    struct epoll_event in_ev, out_events[MAX_CLIENTS_S];
+    int mepoll_fd, nfds;
 
+    mepoll_fd = epoll_create1(0);
+    if (mepoll_fd == -1) {
+        printf("epoll_create1 error.\n");
+        return -1;
+    }
 
     while (true) {
 
